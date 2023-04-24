0129154713e39fbabccfdfbebff461656ffeec2d
amy <homeamy29@gmail.com>
2023-04-24 10:18:52 +0800

    Init project

--- app/src/main/java/fcu/app/breakfast/MainActivity.java
+++ app/src/main/java/fcu/app/breakfast/MainActivity.java
@@ -3,7 +3,9 @@ package fcu.app.breakfast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
 public class MainActivity extends AppCompatActivity {
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
