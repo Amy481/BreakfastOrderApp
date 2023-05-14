470fcb44866f60ab46d9cdc5745d1fc0a23932eb
Amy <homeamy29@gmail.com>
2023-05-14 21:20:58 +0800

    menu頁基本架構完成

--- app/src/main/java/fcu/app/breakfast/Menu.java
+++ app/src/main/java/fcu/app/breakfast/Menu.java
@@ -13,21 +13,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 import fcu.app.breakfast.ui.main.SectionsPagerAdapter;
 import fcu.app.breakfast.databinding.ActivityMenuBinding;
 
 public class Menu extends AppCompatActivity {
 
   private ActivityMenuBinding binding;
-
-  private MenuDatabase manudata;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -41,34 +32,6 @@ public class Menu extends AppCompatActivity {
     viewPager.setAdapter(sectionsPagerAdapter);
     TabLayout tabs = binding.tabs;
     tabs.setupWithViewPager(viewPager);
-
-    // 傳入菜單(建立菜單資料庫)
-    // 開啟資料庫
-    manudata = new MenuDatabase(this);
-    manudata.open();
-    // 讀取檔案((檔案要放在需要的位置
-    File prjDir = this.getFilesDir();
-    File manuText = new File(prjDir, "manu.txt");
-
-    try {
-      FileInputStream fis = new FileInputStream(manuText);
-      InputStreamReader isr = new InputStreamReader(fis);
-      BufferedReader br = new BufferedReader(isr);
-
-      String line = "";
-      StringBuilder sbContent = new StringBuilder();
-      // 讀取檔案資料
-      while ((line = br.readLine()) != null){
-        sbContent.append(line);
-        String[] temp = sbContent.toString().split(" "); // 切片
-        manudata.addMeal(temp[0], temp[1], temp[2], Integer.valueOf(temp[3]), temp[4]);
-      }
-
-    } catch (FileNotFoundException e){
-      throw new RuntimeException(e);
-    } catch (IOException e){
-      throw new RuntimeException(e);
-    }
 
   }
 }
